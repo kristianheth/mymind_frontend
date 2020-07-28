e47b029680db4bbed129eaf1b942024fe5260d64
Kristian Hetherington <62567949+kristianheth@users.noreply.github.com>
2020-07-28 18:13:02 +0200

    implement login form with props fixed

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const LoginForm = () => {
+const LoginForm = (props) => {
   return (
     <div className="login-form">
       <input
